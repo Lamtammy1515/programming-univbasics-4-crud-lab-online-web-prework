b8735ce4062f1b2b0ae9f3b2499ebf774b05ca5d
Tammy Lam <lamtammy1515@gmail.com>
2020-01-21 04:26:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["Macys", "Nordstrom", "Jcpenney", "array!"]
-  array.pop
+  array.pop("array!")
 end
 
 def remove_element_from_start_of_array(array)
