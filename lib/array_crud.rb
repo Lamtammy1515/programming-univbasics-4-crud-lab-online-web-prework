0a992ada4f1cdded977a89601b305d7a5986547f
Tammy Lam <lamtammy1515@gmail.com>
2020-01-21 04:23:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = 
+  array = ["Macys", "Nordstrom", "Jcpenney", "Sears"]
+  array.push("Garage")
 end
 
 def add_element_to_start_of_array(array, element)
